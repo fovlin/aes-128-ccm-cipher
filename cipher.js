f2451dec51c9905be1f1d5694cac5d59d52466ac
Fovlin <fovlin@163.com>
2026-06-29 11:07:07 +0800

    Update cipher.js

--- cipher.js
+++ cipher.js
@@ -6,9 +6,8 @@ export function encrypt(algorithm, plaintext, keyLength, authTagLength) {
     crypto.generateKey("aes",{ length:keyLength },(err,key) => {
         if (err) throw err;
         var cipher = crypto.createCipheriv(algorithm,key,iv,{authTagLength:authTagLength});
-        const ciphertext = cipher.update(plaintext, "utf-8", "hex");
-        cipher.final();
-        const tag = cipher.getAuthTag();
+        const ciphertext = cipher.update(plaintext, "utf-8", "hex") + cipher.final("hex"),
+        tag = cipher.getAuthTag();
         console.log("Ciphertext: " + ciphertext);
         console.log("Algorithm: " + algorithm);
         console.log("Key: " + key.export().toString("hex"));
@@ -21,32 +20,53 @@ export function encrypt(algorithm, plaintext, keyLength, authTagLength) {
 export function decrypt(algorithm, ciphertext, key, iv, tag, authTagLength) {
     var deCiphertext = crypto.createDecipheriv(algorithm, Buffer.from(key,"hex"), Buffer.from(iv,"hex"), {authTagLength:authTagLength});
     deCiphertext.setAuthTag(Buffer.from(tag,"hex"));
-    const context = deCiphertext.update(ciphertext, "hex", "utf-8");
-    deCiphertext.final();
+    const context = deCiphertext.update(ciphertext, "hex", "utf-8") + deCiphertext.final("utf-8");
     console.log(context);
     return context;
 }
 
 export function encryptFile(algorithm, file, keyLength, authTagLength) {
-    const context = fs.readFileSync(file)
-    const iv = crypto.randomBytes(12);
+    const fileData = fs.readFileSync(file),
+    iv = crypto.randomBytes(12);
     crypto.generateKey("aes",{ length:keyLength },(err,key) => {
         if (err) throw err;
         var cipher = crypto.createCipheriv(algorithm,key,iv,{authTagLength:authTagLength});
-        const result = cipher.update(context);
+        const cipherData = cipher.update(fileData);
         cipher.final();
         const tag = cipher.getAuthTag();
-        outPut(algorithm, result, key, iv, tag, authTagLength);
-        fs.writeFileSync("cipher.sec",result);
+        fs.writeFileSync(file + ".enc",cipherData);
+        console.log("Algorithm: " + algorithm);
+        console.log("Key: " + key.export().toString("hex"));
+        console.log("Iv: " + iv.toString("hex"));
+        console.log("Tag: " + tag.toString("hex"));
+        console.log("AuthTagLength: " + authTagLength);
+        const secData = Buffer.from(
+            JSON.stringify(
+                {
+                    "algorithm": algorithm,
+                    "key": key.export().toString("hex"),
+                    "iv": iv.toString("hex"),
+                    "tag": tag.toString("hex"),
+                    "authTagLength": authTagLength
+                }
+            )
+        )
+        fs.writeFileSync(file + ".key", secData)
     })
 }
 
-export function decipherFile(algorithm, file, key, iv, tag, authTagLength) {
-    const ciphertext = fs.readFileSync(file)
+export function decipherFile(file, srcFile) {
+    const ciphertext = fs.readFileSync(file),
+    secText = JSON.parse(fs.readFileSync(srcFile).toString()),
+    algorithm = secText["algorithm"],
+    key = secText["key"],
+    iv = secText["iv"],
+    tag = secText["tag"],
+    authTagLength = secText["authTagLength"]
     var deCiphertext = crypto.createDecipheriv(algorithm, Buffer.from(key,"hex"), Buffer.from(iv,"hex"), {authTagLength:authTagLength});
     deCiphertext.setAuthTag(Buffer.from(tag,"hex"));
     const context = deCiphertext.update(ciphertext);
     deCiphertext.final();
     console.log(context);
-    fs.writeFileSync("output",context)
+    fs.writeFileSync(file.replace(".enc",""),context)
 }
